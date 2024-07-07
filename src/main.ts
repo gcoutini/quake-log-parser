e32a1591ef3d77c72e620780b8566096bc8b67f6
Gabriel Coutini <gcoutini@hotmail.com>
2024-07-07 15:48:42 -0300

    chore: config de ambiente e dependencias

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: console,
+  });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Cloudwalk - Quake Log Parser (gcoutini)')
+    .setDescription('API para o parser de logs do Quake')
+    .setContact('gcoutini', 'https://github.com/gcoutini', 'gcoutini@hotmail.com')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
